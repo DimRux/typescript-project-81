c4055b43bc29bc99b64d0f301163eebb1a7f6be4
dimrux <dimrux@gmail.com>
2024-04-21 11:17:48 +0300

    1 step v1

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const sum_1 = tslib_1.__importDefault(require("./sum"));
-console.log((0, sum_1.default)(3, 3));
-//# sourceMappingURL=index.js.map
--- dist/sum.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sum = (a, b) => a + b;
-exports.default = sum;
-//# sourceMappingURL=sum.js.map
